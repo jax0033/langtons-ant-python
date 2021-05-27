f971ac4e8ac701db65169d82a6600131726a5128
jax0033 <jax0033@protonmail.com>
2021-05-27 20:49:43 +0200

    Remade langton.py
    + performance upgrades
    + improved readability
    + improved rule changing

--- langton.py
+++ langton.py
@@ -1,124 +1,111 @@
 import pygame
-import random
+import random as r
 import os
-
-randcolors = [(random.randint(0,255),random.randint(0,255),random.randint(0,255)) for i in range(33)]
-
-os.environ["SDL_VIDEO_WINDOW_POS"] = "700,35"
+#here you can decide for the rules
+rules = "rl"
+#initialize pygame and set window position
+os.environ["SDL_VIDEO_WINDOW_POS"] = "500,35"
 pygame.init()
-width,heigth = 1000,1000
-screen = pygame.display.set_mode((width,heigth))
+#returns color for the rules
+def get_random_color():
+	return (r.randint(0,255),r.randint(0,255),r.randint(0,255))
 
 class Board:
+	def __init__(self,w,h,rules):
+		#declares width height, sets all the rules as colors in list which indexes will be used as ids
+		self.w = w
+		self.h = h
+		self.rules = rules
+		self.color = [get_random_color() for n in range(len(rules))]
+		self.running = False
+		#ant position in the middle of the board
+		self.ant = [w//2,h//2]
+		#declares movement information for the ant
+		self.index = 0
+		self.orientation = 3
+		self.or_index = [[-1,0],[0,1],[1,0],[0,-1]]
+		self.length = len(rules)-1
+		self.screen = pygame.display.set_mode((self.w,self.h))
 
-	class Cursor:
-		def __init__(self,board):
-			board = board
-			self.pos = [len(board)//2, 
-						len(board[0])//2]
-			self.directions = ("up","right","down","left")
-			self.direction = 3
+	#adds values of same size lists
+	def add(self,a,b):
+		a[0] = a[0]+b[0]
+		a[1] = a[1]+b[1]
+		return a
 
-		def move_right(self):
-			if self.direction == 3:
-				self.direction = 0
-			else: 
-				self.direction += 1
-			self.move()
+	
+	def move_right(self):
+		#choses next available color
+		if self.index+1 > self.length:
+			self.index = 0
+		else:
+			self.index += 1
 
-		def move_left(self):
-			if self.direction == 0:
-				self.direction = 3
-			else:
-				self.direction -= 1
-			self.move()
+		self.draw(self.ant[0],self.ant[1],self.color[self.index])
 
-		def move(self):
-			if self.direction == 0:
-				self.pos = [self.pos[0],self.pos[1]-1]
-			elif self.direction == 1:
-				self.pos = [self.pos[0]+1,self.pos[1]]
-			elif self.direction == 2:
-				self.pos = [self.pos[0],self.pos[1]+1]
-			elif self.direction == 3:
-				self.pos = [self.pos[0]-1,self.pos[1]]
+		#changes movement vector of the ant
+		self.orientation += 1
+		if self.orientation > 3:
+			self.orientation = 0
+		self.ant = self.add(self.ant,self.or_index[self.orientation])
 
-	def __init__(self):
-		self.board = [[0 for i in range(1000)] for i in range(1000)]
-		self.cursor = self.Cursor(self.board)
-		#self.squares = [(0, 0,  (255,255,255)),
-		#				(1, 1,  (0,0,0)), 
-		#				(2, 0,  (34,178,255)),
-		#				(3, 1,  (81,163,31)),
-		#				(4,)]
-		self.squares = [(n,0,randcolors[n]) for n in range(len("RLLLLLRRL"))]
-#RLLLLLRRL
-	def act(self):
-		tile_id = self.board[self.cursor.pos[0]][self.cursor.pos[1]] 
-		if tile_id == 0:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 1
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[1][2])
-			self.cursor.move_right()
-		
-		elif tile_id == 1:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 2
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[2][2])
-			self.cursor.move_left()
-		
-		elif tile_id == 2:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 3
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[3][2])
-			self.cursor.move_left()
+	
+	def move_left(self):
+		#choses next available color
 
-		elif tile_id == 3:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 4
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[4][2])
-			self.cursor.move_left()
-		
-		elif tile_id == 4:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 5
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[5][2])
-			self.cursor.move_left()
+		if self.index+1 > self.length:
+			self.index = 0
+		else:
+			self.index += 1
 
-		elif tile_id == 5:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 6
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[6][2])
-			self.cursor.move_left()
-		
-		elif tile_id == 6:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 7
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[7][2])
-			self.cursor.move_right()
+		self.draw(self.ant[0],self.ant[1],self.color[self.index])
 
-		elif tile_id == 7:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 8
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[8][2])
-			self.cursor.move_right()
-		
-		elif tile_id == 8:
-			self.board[self.cursor.pos[0]][self.cursor.pos[1]] = 0
-			self.draw(self.cursor.pos[0],self.cursor.pos[1],self.squares[0][2])
-			self.cursor.move_left()
+		self.orientation -= 1
+		if self.orientation < 0:
+			self.orientation = 3
+
+		self.ant = self.add(self.ant,self.or_index[self.orientation])
+
+	
+	def format(self):
+		#checks if the ant goes right or left
+		if self.rules[self.index] == "r":
+			self.move_right()
+			return
+		if self.rules[self.index] == "l":
+			self.move_left()
+			return
+
+	
+	def iterate(self):
+		#gets the color of the field the ant is standing on
+		ANT_POS = self.screen.get_at(self.ant)
+		ANT_POS = ANT_POS[0],ANT_POS[1],ANT_POS[2]
+		#checks in colors for the color to get the index / id of the color
+		for ind,color_id in enumerate(self.color):
+			if ANT_POS == color_id:
+
+				self.index = ind
+				self.format()
+				return
+
 	
 	def draw(self,x,y,color):
-		pygame.draw.rect(screen,color,pygame.Rect(x*1,y*1,1,1))
+		self.screen.set_at((x,y),color)
 
-def main():
-	fps = 500
-	clock = pygame.time.Clock()
-	running = True
-	board = Board()
-	screen.fill((255,255,255))
-	pygame.display.update()
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
+	
+	def run (self):
+		#fills the screen with the first color so the ant can begin moving
+		self.screen.fill(self.color[0])
+		self.running = True
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					quit()
+			self.iterate()
+			pygame.display.update()
 
-		board.act()
-
-		pygame.display.update()
-		#clock.tick(fps)
-
-
-main()
+if __name__ == "__main__":
+	#window size and rules
+	b = Board(1000,1000,rules)
+	b.run()
